64aece222e8fe4dffb2d157799192756485b4e90
Hernan Galante <hernan_galante@hotmail.com>
2020-02-18 17:42:09 -0600

    Adding more memory to the executors.

--- process_xml.py
+++ process_xml.py
@@ -6,16 +6,21 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession\
     .builder\
+    .config('spark.jars.packages', 'com.databricks:spark-xml_2.11:0.8.0') \
+    .config('spark.executor.memory', '16g') \
+    .config('spark.executor.cores', '6') \
+    .config('spark.cores.max', '8') \
+    .config('spark.driver.memory','24g') \
     .appName("XML")\
     .getOrCreate()
 sc = spark.sparkContext
 
 # Read the schema.
-df = spark.read.format('xml').options(rowTag='book').load('/samples/books.xml')
+df = spark.read.format('xml').options(rowTag='BILL').load('/tmp/xml/add.PER.20191100050000137260000000002_R.xml')
 df.select("author", "_id").write \
     .format('xml') \
-    .options(rowTag='book', rootTag='books') \
-    .save('newbooks.xml')
+    .options(rowTag='BILL', rootTag='BILLS') \
+    .save('/tmp/xml/new_bill.xml')
 
 spark.stop()
 
